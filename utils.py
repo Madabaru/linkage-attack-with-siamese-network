6f639ab4c5174f848d5e8890951bd2e5986fb367
Felix John <john@axantu.com>
2022-05-09 10:01:57 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,15 +3,27 @@ import pandas as pd
 import category_encoders as ce
 import random
 import logging
+import pickle
+import collections
 
 from sklearn.preprocessing import MinMaxScaler
+from collections import OrderedDict
 
 def load_data(args: dict) -> dict:
-    """ Loads the data and transforms it. """
+    """Loads the data and transforms it."""
 
     if "browsing" in args.path:
        
-        df = pd.read_csv(args.path, delimiter=",")
+        df = pd.read_csv(args.path, delimiter=",") 
+
+        df["date"] = pd.to_datetime(df["timestamp"], unit="s")
+        df["weekday"] = df["date"].dt.weekday
+        df["hour"] = df["date"].dt.hour
+
+        weekday_encoder = ce.BinaryEncoder(cols="weekday", return_df=False)
+        weekday_encoded = weekday_encoder.fit_transform(df["weekday"])
+        hour_encoder = ce.BinaryEncoder(cols="hour", return_df=False)
+        hour_encoded = hour_encoder.fit_transform(df["hour"])
         
         gender_encoder = ce.OneHotEncoder(cols="gender", return_df=False)
         gender_encoded = gender_encoder.fit_transform(df["gender"])
@@ -27,29 +39,32 @@ def load_data(args: dict) -> dict:
         user_id = df["user_id"].values
         timestamp = df["timestamp"].values
 
-        data = np.column_stack((user_id, timestamp, gender_encoded, url_encoded, cat_encoded, domain_encoded, age_encoded))
+        data = np.column_stack((user_id, timestamp, gender_encoded, url_encoded, cat_encoded, domain_encoded, age_encoded)) 
         embedding_dim = data.shape[1]
         args.embedding_dim = embedding_dim - 2
+        logging.info("Embedding Dimesion: %i", args.embedding_dim) 
 
         prev_time = 0.0
         start_time = data[0, 1]
+        trace_lengths = []
 
-        user_to_traces_map = {}
+        user_to_traces_map = OrderedDict()
         trace = []
 
         for i in range(0, data.shape[0]):
-
             if not int(data[i, 0]) in user_to_traces_map: 
                 user_to_traces_map[int(data[i, 0])] = []
                 if len(trace) >= args.min_trace_len:
                     if data[i - 1, 1] - start_time <= args.max_trace_duration:
+                        trace_lengths.append(len(trace))
+                        trace = pad_trace(args, trace)
                         user_to_traces_map[int(data[i - 1, 0])].append(trace)
                 trace = []
                 start_time = data[i, 1]
-
             if len(trace) >= args.max_trace_len or (prev_time != 0.0 and data[i, 1] - prev_time >= args.max_delay):
                 if len(trace) >= args.min_trace_len and data[i - 1, 1] - start_time <= args.max_trace_duration:
                     trace = pad_trace(args, trace)
+                    trace_lengths.append(len(trace))
                     user_to_traces_map[int(data[i, 0])].append(trace)
                 trace = []
                 start_time = data[i, 1]
@@ -58,12 +73,20 @@ def load_data(args: dict) -> dict:
             prev_time = data[i, 1]
             prev_user = int(data[i, 0])
 
+        trace = pad_trace(args, trace)
+        trace_lengths.append(len(trace))
+        user_to_traces_map[int(data[-1, 0])].append(trace)
+        logging.info("Average trace length: %i", np.mean(trace_lengths)) 
+        
         del data    
-        logging.info("Number of clients before: %i", len(user_to_traces_map.keys())) 
+        user_to_traces_map = {i: v[1] for i, v in enumerate(user_to_traces_map.items())}
+
+        logging.info("Number of users before: %i", len(user_to_traces_map.keys())) 
         filtered_user_to_traces_map = {k: v for k, v in user_to_traces_map.items() if len(v) >= args.min_num_traces_per_user} 
-        logging.info("Number of clients after: %i", len(filtered_user_to_traces_map.keys())) 
+        logging.info("Number of users after: %i", len(filtered_user_to_traces_map.keys())) 
         num_samples = sum([len(v) for k, v in filtered_user_to_traces_map.items()])
         logging.info("Total number of traces: %i", num_samples) 
+
         return filtered_user_to_traces_map
     
     else:
@@ -124,16 +147,23 @@ def load_data(args: dict) -> dict:
             fields.append(village_encoded)
         if "suburb" in args.fields:
             fields.append(suburb_encoded)
+        if "speed" in args.fields:
+            fields.append(speed)
+        if "heading" in args.fields:
+            fields.append(heading)
 
         data = np.column_stack(fields)
         embedding_dim = data.shape[1]
         args.embedding_dim = embedding_dim - 2
+        logging.info("Embedding Dimesion: %i", args.embedding_dim) 
 
         prev_time = 0.0
         start_time = data[0, 1]
 
-        user_to_traces_map = {}
+        user_to_traces_map = OrderedDict()
+        trace_lengths = []
         trace = []
+        trace_coordinates = []
 
         for i in range(0, data.shape[0]):
 
@@ -141,12 +171,15 @@ def load_data(args: dict) -> dict:
                 user_to_traces_map[int(data[i, 0])] = []
                 if len(trace) >= args.min_trace_len:
                     if data[i - 1, 1] - start_time <= args.max_trace_duration:
+                        trace_lengths.append(len(trace))
+                        trace = pad_trace(args, trace)
                         user_to_traces_map[int(data[i - 1, 0])].append(trace)
                 trace = []
                 start_time = data[i, 1]
 
             if len(trace) >= args.max_trace_len or (prev_time != 0.0 and data[i, 1] - prev_time >= args.max_delay):
                 if len(trace) >= args.min_trace_len and data[i - 1, 1] - start_time <= args.max_trace_duration:
+                    trace_lengths.append(len(trace))
                     trace = pad_trace(args, trace)
                     user_to_traces_map[int(data[i, 0])].append(trace)
                 trace = []
@@ -157,6 +190,9 @@ def load_data(args: dict) -> dict:
             prev_user = int(data[i, 0])
 
         del data    
+        user_to_traces_map = {i: v[1] for i, v in enumerate(user_to_traces_map.items())}
+        
+        logging.info("Average trace length: %i", np.mean(trace_lengths))         
         logging.info("Number of clients before: %i", len(user_to_traces_map.keys())) # Rust 7933
         filtered_user_to_traces_map = {k: v for k, v in user_to_traces_map.items() if len(v) >= args.min_num_traces_per_user} 
         logging.info("Number of clients after: %i", len(filtered_user_to_traces_map.keys())) # Rust: 3861
@@ -166,26 +202,45 @@ def load_data(args: dict) -> dict:
 
 
 def pad_trace(args: dict, trace: list) -> list:
-    """ Adds padding a given trace. """
+    """Adds padding a given trace."""
     if len(trace) < args.max_trace_len:
         diff = args.max_trace_len - len(trace)
         trace.extend([np.zeros((args.embedding_dim)) for i in range(diff)])
     return trace
 
 
-def load_map(path: str) -> dict:
-    """ Loads map. """
-    with open(path, "rb") as file:
-        map = pickle.load(file)
-    return map
-
-
-def gen_target_user_to_target_trace_map(user_to_traces_map: dict, target_user_list: list) -> dict:
-    """ Generates a map with target user as key and target traces as value. """
-    target_user_to_target_trace_map = {}
+def gen_user_to_target_idx_map(args: dict, user_to_traces_map: dict):
+    """Generates a dictionary with random target traces indices (value) for each user (key)."""
+    user_to_target_idx_map = {}
+    target_user_list = random.sample(user_to_traces_map.keys(), args.sample_size)
     for target_user in target_user_list:
-        traces_list = user_to_traces_map[target_user]
-        split = int(len(traces_list) / 2)
-        target_user_to_target_trace_map[target_user] = random.sample(traces_list[split:], 1)[0]
-    return target_user_to_target_trace_map
+        traces_list = user_to_traces_map.get(target_user)
+        start_idx = len(traces_list[int(len(traces_list) / 2):])
+        end_idx = len(traces_list)
+        target_index_list = list(range(start_idx, end_idx))
+        if len(target_index_list) < args.num_target_traces:
+            sampled_target_idx_list = random.sample(target_index_list, len(target_index_list))
+        else:
+            sampled_target_idx_list = random.sample(target_index_list, args.num_target_traces)
+        user_to_target_idx_map[target_user] = sampled_target_idx_list
+    return user_to_target_idx_map
 
+
+def gen_user_to_sample_idx_map(user_to_traces_map: dict, k: int):
+    """Generates a dictionary with random observed trace indicies (value) for each user (key)."""
+    user_to_sample_idx_map = {}
+    for client, traces_list in user_to_traces_map.items():
+        if k == 0:
+            user_to_sample_idx_map[client] = list(range(len(traces_list[:int(len(traces_list) / 2)])))
+        else:
+            if k > int(len(traces_list) / 2):
+                user_to_sample_idx_map[client] = list(range(len(traces_list[:int(len(traces_list) / 2)])))
+            else:
+                user_to_sample_idx_map[client] = list(range(len(traces_list[:k])))
+    return user_to_sample_idx_map
+
+
+def gen_user_to_test_idx_map(user_to_sample_idx_map: dict):
+    """Generates a dictionary with a single random observed trace index (value) for each user (key)."""
+    user_to_test_idx_map = {client: random.sample(traces_list, 1)[0] for client, traces_list in user_to_sample_idx_map.items()}
+    return user_to_test_idx_map
